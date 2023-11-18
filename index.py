58038f2c3f2ed74341b6c59aaf45922abf935216
Evan Harris <evan.harris@bjss.com>
2023-11-18 11:13:56 +0000

    everything

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import (Flask, request, Response, render_template)
+from dotenv import load_dotenv
+
+load_dotenv()
+#openai.api_key = os.environ.get('GPT_API_KEY')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
